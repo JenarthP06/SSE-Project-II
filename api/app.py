c448f38225d49d4328201e3cf3dd6e43cf03c6e0
rayqg22 <72386070+rayqg22@users.noreply.github.com>
2024-03-02 20:53:18 +0000

    Formatting fix

--- api/app.py
+++ api/app.py
@@ -12,16 +12,6 @@ key = os.environ.get("supabase_key")
 
 supabase = create_client(url, key)
 
-# @app.route('/')
-# def index():
-#     response = supabase.table('users').select('*').execute()
-#     data = response.get('data')
-#     error = response.get('error')
-
-#     if error:
-#         return {"error": str(error)}, 500
-#     return jsonify({'data': data})
-
 
 @app.route('/')
 def index():
@@ -32,43 +22,6 @@ def index():
         data = []
 
     return render_template('index.html', users=data)
-
-# # Initialize Supabase client
-# url = os.environ.get("supabase_url")
-# key = os.environ.get("supabase_key")
-# supabase = create_client(url, key)
-
-# # Check if the username already exists
-# data, count = supabase.table('users').select('*').execute()
-
-# db = SQLAlchemy(app)
-
-# # Storing credentials
-# @app.route('/store_credentials', methods=['POST'])
-# def store_credentials():
-#     data = request.get_json()
-
-#     # Extract username and password from the request data
-#     username = data.get('username')
-#     password = data.get('password')
-
-#     if username and password:
-#         # Simulate storing user credentials in the Supabase database
-#         new_user_data = {"username": username, "password": password}
-#         supabase.table('user_credentials').upsert([new_user_data], on_conflict=['username'])
-
-#         return jsonify({"message": "Credentials stored successfully"}), 201
-#     else:
-#         return jsonify({"error": "Missing username or password"}), 400
-
-# # Presenting user information
-# @app.route('/user_profile/<user_id>', methods=['GET'])
-# def get_user_profile(user_id):
-#     user = users.get(user_id)
-#     if user:
-#         return jsonify({"username": user.get("username", "N/A")})
-#     else:
-#         return jsonify({"error": "User not found"}), 404
 
 
 if __name__ == '__main__':
