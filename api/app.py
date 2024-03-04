9ca9f07154ad0df3dc65761f6a2e85cfed9868d4
rayqg22 <72386070+rayqg22@users.noreply.github.com>
2024-03-04 14:50:18 +0000

    Working event queue

--- api/app.py
+++ api/app.py
@@ -18,32 +18,4 @@ def index():
 
 if __name__ == '__main__':
     app.run(debug=True)
-
-# url = os.environ.get("SUPABASE_URL")
-# key = os.environ.get("SUPABASE_KEY")
-
-
-# def create_supabase_client():
-#     url = os.environ.get("SUPABASE_URL")
-#     key = os.environ.get("SUPABASE_KEY")
-#     return create_client(url, key)
-
-
-# supabase = create_supabase_client()
-
-# # supabase = create_client(url, key)
-
-
-# @app.route('/')
-# def index():
-#     response = supabase.table('users').select('*').execute()
-#     if hasattr(response, 'data'):
-#         data = response.data
-#     else:
-#         data = []
-
-#     return render_template('index.html', users=data)
-
-
-# if __name__ == '__main__':
-#     app.run(debug=True)
+    
--- api/service_bus_listener.py
+++ api/service_bus_listener.py
@@ -10,24 +10,23 @@ supabase = create_client(os.environ.get("SUPABASE_URL"), os.environ.get("SUPABAS
 
 def insert_user_data(username):
     response = supabase.table('user-profile').insert({"username": username}).execute()
-    if response.error:
+    if hasattr(response, 'error') and response.error:
         print(f"Error updating user {username}: {response.error}")
     else:
         print(f"User {username} updated successfully.")
 
+
 def process_service_bus_messages():
     service_bus_client = ServiceBusClient.from_connection_string(conn_str=os.environ.get("SERVICE_BUS_CONNECTION_STRING"))
     with service_bus_client:
-        receiver = service_bus_client.get_subscription_receiver(
-            topic_name='user-events',
-            subscription_name='user-profile-updates'
-        )
+        receiver = service_bus_client.get_queue_receiver(queue_name='userprofile-queue')
         with receiver:
             for msg in receiver:
                 try:
-                    message_body = json.loads(msg.body.decode('utf-8'))
+                    message_body_bytes = b''.join(b for b in msg.body)
+                    message_body_str = message_body_bytes.decode('utf-8')
+                    message_body = json.loads(message_body_str)
                     username = message_body['username']
-
                     insert_user_data(username)
                     receiver.complete_message(msg)
                 except Exception as e:
