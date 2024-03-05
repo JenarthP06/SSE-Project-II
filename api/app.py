cee1348204522927ba19b937e75ecdc0e2baad18
Sach <sach@dyn3166-56.wlan.ic.ac.uk>
2024-03-05 19:19:45 +0000

    containerized

--- api/app.py
+++ api/app.py
@@ -110,4 +110,4 @@ api.add_resource(DeleteFavourite, '/deletefavourite')
 api.add_resource(Favourite, '/displayfavourite')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
